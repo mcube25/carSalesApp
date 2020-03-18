26698277284b26f9e8db40a46052dc53362de1aa
Muhammad Muhktar Musa <mcube3567@gmail.com>
2020-03-18 13:46:12 -0700

    td forms

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -3,7 +3,6 @@ import { Subscription } from 'rxjs';
 
 import { Ingredient } from '../shared/ingredient.model';
 import { ShoppingListService } from './shopping-list.service';
-import { LoggingService } from '../logging.service';
 
 @Component({
   selector: 'app-shopping-list',
@@ -14,20 +13,16 @@ export class ShoppingListComponent implements OnInit, OnDestroy {
   ingredients: Ingredient[];
   private subscription: Subscription;
 
-  constructor(
-    private slService: ShoppingListService,
-    private loggingService: LoggingService
-  ) {}
+  constructor(private slService: ShoppingListService) { }
 
   ngOnInit() {
     this.ingredients = this.slService.getIngredients();
-    this.subscription = this.slService.ingredientsChanged.subscribe(
-      (ingredients: Ingredient[]) => {
-        this.ingredients = ingredients;
-      }
-    );
-
-    this.loggingService.printLog('Hello from ShoppingListComponent ngOnInit!');
+    this.subscription = this.slService.ingredientsChanged
+      .subscribe(
+        (ingredients: Ingredient[]) => {
+          this.ingredients = ingredients;
+        }
+      );
   }
 
   onEditItem(index: number) {
